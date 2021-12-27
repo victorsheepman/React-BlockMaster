5df1487ebaffead51d54349de59ac14412458e17
unknown <chirlychplin@gmail.com>
2021-12-27 14:27:15 -0400

    modal

--- src/components/Carousel/Carousel.jsx
+++ src/components/Carousel/Carousel.jsx
@@ -1,24 +1,24 @@
 import React from 'react'
 export const Carousel = () => (
-        <div id="carouselExampleControls" class="carousel slide " data-bs-ride="carousel">
-            <div class="carousel-inner">
-                <div class="carousel-item active">
-                    <img src="assets/images/unidos.jpg" class="d-block w-100 image" alt="..."/>
+        <div id="carouselExampleControls" className="carousel slide caorusel-index " data-bs-ride="carousel" tabIndex="1">
+            <div className="carousel-inner">
+                <div className="carousel-item active">
+                    <img src="assets/images/unidos.jpg" className="d-block w-100 image" alt="..."/>
                 </div>
-                <div class="carousel-item ">
-                    <img src="assets/images/mulan.jpg" class="d-block w-100 image" alt="..."/>
+                <div className="carousel-item ">
+                    <img src="assets/images/mulan.jpg" className="d-block w-100 image" alt="..."/>
                 </div>
-                <div class="carousel-item">
-                    <img src="assets/images/raya.jpg" class="d-block w-100 image" alt="..."/>
+                <div className="carousel-item">
+                    <img src="assets/images/raya.jpg" className="d-block w-100 image" alt="..."/>
                 </div>
             </div>
-            <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="prev">
-                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-                <span class="visually-hidden">Previous</span>
+            <button className="carousel-control-prev " type="button" data-bs-target="#carouselExampleControls" data-bs-slide="prev">
+                <span className="carousel-control-prev-icon" aria-hidden="true"></span>
+                <span className="visually-hidden">Previous</span>
             </button>
-            <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="next">
-                <span class="carousel-control-next-icon" aria-hidden="true"></span>
-                <span class="visually-hidden">Next</span>
+            <button className="carousel-control-next " type="button" data-bs-target="#carouselExampleControls" data-bs-slide="next">
+                <span className="carousel-control-next-icon" aria-hidden="true"></span>
+                <span className="visually-hidden">Next</span>
             </button>
         </div>
 )
--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -1,40 +1,35 @@
 // @flow 
 import React from 'react';
-import ReactDOM from 'react-dom';
-
 export const Modal = (props) => {
     const closeModal =(event)=>{
         props.setState(false);
         event.stopPropagation();
     }
-    return ReactDOM.createPortal (
-        <div className='modal'>
-            <div className='modal__container'>
-                <article className='modal__info'>
-                    <h1 className='modal__title'>{props.title}</h1>
-                    <span className='modal__overview'>{props.overview}</span>
-                    <div className='modal__date'></div>
-                    <div className='modal__wrapperButton'>
-                        <button className='modal__button'>VER AHORA</button>
-                        <button className='modal__button--black'>
+    return(
+        <div className='modalMovie'>
+            <div className='modalMovie__container'>
+                <article className='modalMovie__info'>
+                    <h1 className='modalMovie__title'>{props.title}</h1>
+                    <span className='modalMovie__overview'>{props.overview}</span>
+                    <div className='modalMovie__date'></div>
+                    <div className='modalMovie__wrapperButton'>
+                        <button className='modalMovie__button'>VER AHORA</button>
+                        <button className='modalMovie__button--black'>
                             VER DESPUÉS
                         </button>
                     </div>
                 </article>
-                <article className='modal__card'>
+                <article className='modalMovie__card'>
                     <img src={`//image.tmdb.org/t/p/w220_and_h330_face${props.poster_path}`} alt="" />
                     <span className={(props.vote_average >=7 && 'recommended') || 'normal'}>
                         {props.vote_average}
                         <img src="assets/icons/Property 1=star.svg" alt="" />
                     </span>
                 </article>
-                <figure className='modal__exit' onClick={closeModal}>
+                <figure className='modalMovie__exit' onClick={closeModal}>
                     <img src="assets/icons/Property 1=delete.svg" alt="" />
                 </figure>
-                
             </div>            
-        </div>,
-   
-        document.getElementById('modal')  
+        </div>
     );
 };
